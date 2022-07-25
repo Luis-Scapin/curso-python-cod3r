659c90aaf99825ef01705fd2702959440b901f89
Luis-Scapin <luis.scapin@gmail.com>
2022-07-24 23:55:52 -0300

    Mais aulas de banco de dados e curso concluído!

---
+++ banco_dados/povoar_grupo.py
@@ -0,0 +1,19 @@
+from mysql.connector.errors import ProgrammingError
+from bd import nova_conexao
+
+sql = 'INSERT INTO grupos(descricao) VALUES (%s)'
+args = (
+    ('Pessoal',),
+    ('Trabalho',),
+    ('Padel',),
+)
+
+with nova_conexao() as conexao:
+    try:
+        cursor = conexao.cursor()
+        cursor.executemany(sql, args)
+        conexao.commit()
+    except ProgrammingError as e:
+        print(f'Erro: {e.msg}')
+    else:
+        print(f'{cursor.rowcount} registro(s) incluído(s)!')
